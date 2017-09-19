0a7559e38dae23d2925932fcb6326f1076ca53b8
unh0lys0daz <unh0lys0daz@gmail.com>
2017-09-19 23:52:37 +0200

    Merged the files

--- main.py
+++
@@ -1,5 +0,0 @@
-import argparse as argp
-
-parser = argp.ArgumentParser(description='Get some bookz')
-parser.add_argument('book_name', metavar='Book', type=string, nargs='+',
-                    help='da book to get')
